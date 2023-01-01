9cf9435f642d55325525926380fde4eac3e6b80e
tuantea <tuan1722000@gmail.com>
2023-01-01 13:18:18 +0700

    update

--- py_scripts/recommender.py
+++ py_scripts/recommender.py
@@ -57,8 +57,15 @@ def get_top_10(df):
     screen_bool = (df.screen >= user.screen_range[0]) \
                   & (df.screen <= user.screen_range[1])
     ram_bool = df.memory == user.ram
-    return df[budget_bool & storage_bool & screen_bool & ram_bool].sort_values('total_score',ascending=False).head(10).sort_values('weight',ascending=False).head(10)
-
+    return df[budget_bool & storage_bool & screen_bool & ram_bool].sort_values('total_score',ascending=False).head(10)
+def get_top_10_weight(df):
+    budget_bool = df.price <= user.budget
+    storage_bool = (df.storage >= user.storage_range[0]) \
+                   & (df.storage <= user.storage_range[1])
+    screen_bool = (df.screen >= user.screen_range[0]) \
+                  & (df.screen <= user.screen_range[1])
+    ram_bool = df.memory == user.ram
+    return df[budget_bool & storage_bool & screen_bool & ram_bool].sort_values('total_score',ascending=False).head(10).sort_values('weight',ascending=True).head(10)
 
 ## MAIN
 # create new columns for scores
@@ -95,16 +102,26 @@ subset = combined_df[combined_df.price <= user.budget + 50]
 #      subset.loc[r, 'memory_score'] = get_memory_score(subset, r)
 #      subset.loc[r, 'screen_score'] = get_screen_score(subset, r)
 #      subset.loc[r, 'total_score'] = sum(subset.loc[r, 'price_score':'screen_score'])
-subset.to_csv('user1.csv')
 # output the results
 top_10 = get_top_10(subset)
-
-if len(top_10) == 0:
-    print("Sorry, we do not have any products that match up to what you are looking for :c")
+top_10_weight=get_top_10_weight(subset)
+if user.sex==1:
+   if len(top_10) == 0:
+      print("Sorry, we do not have any products that match up to what you are looking for :c")
+   else:
+      print("Alright, after considering all the factors that you answered for us, we decided on this list of the top " + str(len(top_10)) + " laptops best suited for you!\n")
+      top_10.to_csv('../data/nam.csv')
+      n = 1
+      for i in top_10.index:
+         print(str(n) + ' - ' + '$' + str(top_10.price[i]) + ' - ' + top_10.name[i])
+         n += 1
 else:
-    print("Alright, after considering all the factors that you answered for us, we decided on this list of the top " + str(len(top_10)) + " laptops best suited for you!\n")
-
-    n = 1
-    for i in top_10.index:
-        print(str(n) + ' - ' + '$' + str(top_10.price[i]) + ' - ' + top_10.name[i])
-        n += 1
+    if len(top_10_weight) == 0:
+      print("Sorry, we do not have any products that match up to what you are looking for :c")
+    else:
+      print("Alright, after considering all the factors that you answered for us, we decided on this list of the top " + str(len(top_10)) + " laptops best suited for you!\n")
+      top_10_weight.to_csv('../data/nu.csv')
+      n = 1
+      for i in top_10_weight.index:
+         print(str(n) + ' - ' + '$' + str(top_10_weight.price[i]) + ' - ' + top_10_weight.name[i])
+         n += 1
