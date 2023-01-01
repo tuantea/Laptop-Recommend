58b7720460c01a6a21f47555605211a22802a57c
tuantea <tuan1722000@gmail.com>
2023-01-01 12:39:42 +0700

    update

--- py_scripts/recommender.py
+++ py_scripts/recommender.py
@@ -19,37 +19,36 @@ def create_new_columns(df):
     df['total_score'] = 0
     return df
 
-def get_price_score(df, row, budget):
+def get_price_score(df, row, budget,priority):
     # get budget from matt's end
     # s(p) = ((b - p) / b) * (1/7)
-    return ((budget - df.loc[row, 'price']) / budget) * (1/7)
-
-def get_rating_score(df, row):
+    return ((budget - df.loc[row, 'price']) / budget) * (priority/28)
+def get_rating_score(df, row,priority):
     rating = df.loc[row, 'rating']
     if rating != -1:
-        return (rating / 5) * (1/7)
+        return (rating / 5) * (priority/28)
     else:
         return 0
 
-def get_cpu_speed_score(df, row):
+def get_cpu_speed_score(df, row,priority):
     max_cpu_speed = 3.1
-    return (df.loc[row, 'cpu_speed'] / max_cpu_speed) * (1/7)
+    return (df.loc[row, 'cpu_speed'] / max_cpu_speed) * (priority/28)
 
-def get_core_score(df, row):
+def get_core_score(df, row,priority):
     max_cores = 6
-    return (df.loc[row, 'cores'] / max_cores) * (1/7)
+    return (df.loc[row, 'cores'] / max_cores) * (priority/28)
 
-def get_storage_score(df, row):
+def get_storage_score(df, row,priority):
     max_storage = 3024
-    return (df.loc[row, 'storage'] / max_storage) * (1/7)
+    return (df.loc[row, 'storage'] / max_storage) * (priority/28)
 
-def get_memory_score(df, row):
+def get_memory_score(df, row,priority):
     max_memory = 32
-    return (df.loc[row, 'memory'] / max_memory) * (1/7)
+    return (df.loc[row, 'memory'] / max_memory) * (priority/28)
 
-def get_screen_score(df, row):
+def get_screen_score(df, row,priority):
     max_screen = 17.3
-    return (df.loc[row, 'screen'] / max_screen) * (1/7)
+    return (df.loc[row, 'screen'] / max_screen) * (priority/28)
 
 def get_top_10(df):
     budget_bool = df.price <= user.budget
@@ -58,27 +57,45 @@ def get_top_10(df):
     screen_bool = (df.screen >= user.screen_range[0]) \
                   & (df.screen <= user.screen_range[1])
     ram_bool = df.memory == user.ram
-    return df[budget_bool & storage_bool & screen_bool & ram_bool].sort_values('total_score',ascending=False).head(10)
+    return df[budget_bool & storage_bool & screen_bool & ram_bool].sort_values('total_score',ascending=False).head(10).sort_values('weight',ascending=False).head(10)
 
 
 ## MAIN
 # create new columns for scores
 study_df = create_new_columns(study_df)
+for r in list(study_df.index):
+     study_df.loc[r, 'price_score'] = get_price_score(study_df, r, user.budget,6)
+     study_df.loc[r, 'rating_score'] = get_rating_score(study_df, r,5)
+     study_df.loc[r, 'cpu_speed_score'] = get_cpu_speed_score(study_df, r,4)
+     study_df.loc[r, 'core_score'] = get_core_score(study_df, r,1)
+     study_df.loc[r, 'storage_score'] = get_storage_score(study_df, r,2)
+     study_df.loc[r, 'memory_score'] = get_memory_score(study_df, r,3)
+     study_df.loc[r, 'screen_score'] = get_screen_score(study_df, r,7)
+     study_df.loc[r, 'total_score'] = sum(study_df.loc[r, 'price_score':'screen_score'])
 gaming_df = create_new_columns(gaming_df)
+for r in list(gaming_df.index):
+     gaming_df.loc[r, 'price_score'] = get_price_score(gaming_df, r, user.budget,3)
+     gaming_df.loc[r, 'rating_score'] = get_rating_score(gaming_df, r,1)
+     gaming_df.loc[r, 'cpu_speed_score'] = get_cpu_speed_score(gaming_df, r,7)
+     gaming_df.loc[r, 'core_score'] = get_core_score(gaming_df, r,5)
+     gaming_df.loc[r, 'storage_score'] = get_storage_score(gaming_df, r,4)
+     gaming_df.loc[r, 'memory_score'] = get_memory_score(gaming_df, r,6)
+     gaming_df.loc[r, 'screen_score'] = get_screen_score(gaming_df, r,2)
+     gaming_df.loc[r, 'total_score'] = sum(gaming_df.loc[r, 'price_score':'screen_score'])
 combined_df = pd.concat([study_df, gaming_df]).reset_index(drop=True)
 
 # do this for each data point - loop thru everything
 subset = combined_df[combined_df.price <= user.budget + 50]
-for r in list(subset.index):
-     subset.loc[r, 'price_score'] = get_price_score(subset, r, user.budget)
-     subset.loc[r, 'rating_score'] = get_rating_score(subset, r)
-     subset.loc[r, 'cpu_speed_score'] = get_cpu_speed_score(subset, r)
-     subset.loc[r, 'core_score'] = get_core_score(subset, r)
-     subset.loc[r, 'storage_score'] = get_storage_score(subset, r)
-     subset.loc[r, 'memory_score'] = get_memory_score(subset, r)
-     subset.loc[r, 'screen_score'] = get_screen_score(subset, r)
-     subset.loc[r, 'total_score'] = sum(subset.loc[r, 'price_score':'screen_score'])
-    #  subset.to_csv('user.csv')
+# for r in list(subset.index):
+#      subset.loc[r, 'price_score'] = get_price_score(subset, r, user.budget)
+#      subset.loc[r, 'rating_score'] = get_rating_score(subset, r)
+#      subset.loc[r, 'cpu_speed_score'] = get_cpu_speed_score(subset, r)
+#      subset.loc[r, 'core_score'] = get_core_score(subset, r)
+#      subset.loc[r, 'storage_score'] = get_storage_score(subset, r)
+#      subset.loc[r, 'memory_score'] = get_memory_score(subset, r)
+#      subset.loc[r, 'screen_score'] = get_screen_score(subset, r)
+#      subset.loc[r, 'total_score'] = sum(subset.loc[r, 'price_score':'screen_score'])
+subset.to_csv('user1.csv')
 # output the results
 top_10 = get_top_10(subset)
 
